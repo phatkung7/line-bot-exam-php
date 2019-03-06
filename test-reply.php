8d4f3d6c560ebe717d04170f4051e6f4d474543a
phatkung7 <phatkung.kizz@gmail.com>
2019-03-06 16:51:58 +0700

    Update test-reply.php

--- test-reply.php
+++ test-reply.php
@@ -42,9 +42,13 @@
         break;
        case "เวร sat wk 10":
             $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
-            $text_msg = "เวร SAT - wk10 มีรายชื่อดังนี้<br>";
-            $text_msg .= "1. พญ.ภาวินี ด้วงเงิน สำนักระบาดวิทยา - Supervisor<br>";
-            $text_msg .= "2. นางสาวธนัญญา สุทธวงค์ สำนักระบาดวิทยา (FETH) - Supervisor Assistant<br>";
+            $text_msg = "เวร SAT - wk10 มีรายชื่อดังนี้";
+            $text_msg .= "1. พญ.ภาวินี ด้วงเงิน สำนักระบาดวิทยา - Supervisor";
+            $text_msg .= "2. นางสาวธนัญญา สุทธวงค์ สำนักระบาดวิทยา (FETH) - Supervisor Assistant";
+            $text_msg .= "3. นางอินท์ฉัตร สุขเกษม สำนักระบาดวิทยา (FETH) - Incharge 1";
+            $text_msg .= "4.นางสาวณัฐกฤตา บริบูรณ์ สำนักโรคไม่ติดต่อ - Incharge 2";
+            $text_msg .= "5.นางสาวกนกอร งามนัก ส.ควบคุมเครื่องดื่มแอลกอฮอล์ - Incharge 2";
+            $text_msg .= "6. นางคัดคนางค์ ศรีพัฒนะพิพัฒน์ กอง.ครฉ. - SAT Manager";
             $arrayPostData['messages'][0]['type'] = "text";
             $arrayPostData['messages'][0]['text'] = $text_msg;
             replyMsg($arrayHeader,$arrayPostData);
