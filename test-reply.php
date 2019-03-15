065fe285f2a0e4a93b242dfc661805c7dab5aca9
phatkung7 <phatkung.kizz@gmail.com>
2019-03-15 12:45:55 +0700

    Update test-reply.php

--- test-reply.php
+++ test-reply.php
@@ -182,18 +182,20 @@
 //         replyMsg($arrayHeader,$arrayPostData);
     }
 
-	if($q="1"){
-	    $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
-            $image_url1 = "https://flu.ddc.moph.go.th/img-bot/dhf-map.png";
-            $arrayPostData['messages'][0]['type'] = "image";
-            $arrayPostData['messages'][0]['originalContentUrl'] = $image_url1;
-            $arrayPostData['messages'][0]['previewImageUrl'] = $image_url1;
-            $image_url2 = "https://flu.ddc.moph.go.th/img-bot/dhf-graph.png";
-            $arrayPostData['messages'][1]['type'] = "image";
-            $arrayPostData['messages'][1]['originalContentUrl'] = $image_url2;
-            $arrayPostData['messages'][1]['previewImageUrl'] = $image_url2;
-            replyMsg($arrayHeader,$arrayPostData);	
-	}else if($q="2"){
+	switch ($q) {
+		case "1" :
+		    $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
+		    $image_url1 = "https://flu.ddc.moph.go.th/img-bot/dhf-map.png";
+		    $arrayPostData['messages'][0]['type'] = "image";
+		    $arrayPostData['messages'][0]['originalContentUrl'] = $image_url1;
+		    $arrayPostData['messages'][0]['previewImageUrl'] = $image_url1;
+		    $image_url2 = "https://flu.ddc.moph.go.th/img-bot/dhf-graph.png";
+		    $arrayPostData['messages'][1]['type'] = "image";
+		    $arrayPostData['messages'][1]['originalContentUrl'] = $image_url2;
+		    $arrayPostData['messages'][1]['previewImageUrl'] = $image_url2;
+		    replyMsg($arrayHeader,$arrayPostData);
+	    break;
+		case "2" :
 	    $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
             $image_url1 = "https://flu.ddc.moph.go.th/img-bot/hfm-map.png";
             $arrayPostData['messages'][0]['type'] = "image";
@@ -204,7 +206,7 @@
             $arrayPostData['messages'][1]['originalContentUrl'] = $image_url2;
             $arrayPostData['messages'][1]['previewImageUrl'] = $image_url2;
             replyMsg($arrayHeader,$arrayPostData);
-	}else{
+	    break;
 	}
 //     case "กรมควบคุมโรค":
 //         $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
