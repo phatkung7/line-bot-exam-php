fe1a59148b4809256be599eef49c749008bb5dbd
phatkung7 <phatkung.kizz@gmail.com>
2019-03-19 14:40:57 +0700

    Update test-reply.php

--- test-reply.php
+++ test-reply.php
@@ -378,14 +378,14 @@ function replyMsg($arrayHeader,$arrayPostData){
         $result = curl_exec($ch);
         curl_close ($ch);
 }
-function Getprofiles($userID){
+function Getprofiles($arrayHeader,$userID){
         $strUrl = "https://api.line.me/v2/bot/profile/{".$userID."}";
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL,$strUrl);
         curl_setopt($ch, CURLOPT_HEADER, false);
-        curl_setopt($ch, CURLOPT_POST, true);
+        //curl_setopt($ch, CURLOPT_POST, true);
         curl_setopt($ch, CURLOPT_HTTPHEADER, $arrayHeader);
-        curl_setopt($ch, CURLOPT_POSTFIELDS,json_encode($arrayPostData));
+        //curl_setopt($ch, CURLOPT_POSTFIELDS,json_encode($arrayPostData));
         curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         $result = curl_exec($ch);
